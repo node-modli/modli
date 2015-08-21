fd23f2736593384e1061e99e94a1a32765dd35cf
fluidbyte <kent@fluidbyte.net>
2015-08-21 12:17:48 -0400

    Remove local db artifact from test setup

--- test/setup.js
+++ test/setup.js
@@ -7,9 +7,3 @@ global.should = require('should')
 global.sinon = sinon
 global.expect = chai.expect
 chai.use(schai)
-
-// Ensure local dynamo is set
-if (!process.env.DYNAMODB_LOCAL) {
-  console.log('Need to set ENV: DYNAMODB_LOCAL');
-  process.exit(1);
-}

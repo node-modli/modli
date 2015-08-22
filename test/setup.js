3c6f795a258b61481fefc17415b9af7d0d397076
fluidbyte <kent@fluidbyte.net>
2015-08-22 10:37:15 -0400

    Removing unused deps

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,4 @@
 import chai from 'chai'
-import sinon from 'sinon'
-import schai from 'sinon-chai'
 global.assert = require('assert')
 global.should = require('should')
-global.sinon = sinon
 global.expect = chai.expect
-chai.use(schai)

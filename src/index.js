1bd0be8b854355b3d0021c61037e43e99e9a2ff7
fluidbyte <kent@fluidbyte.net>
2015-08-22 10:04:15 -0400

    Start adapter lib, expose model at entry point

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { model, Joi } from './lib/model';
+
 /*
  * Copyright (c) 2015 TechnologyAdvice
  */
@@ -6,5 +8,5 @@
  * Entry point for the module, exports methods of the libs
  */
 export default {
-
+  model, Joi
 };

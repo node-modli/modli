7dc8a30dab66daaf4915b6e2fab032798524f085
fluidbyte <kent@fluidbyte.net>
2015-08-23 10:07:31 -0400

    Writing integ test for model creation

--- test/index.js
+++ test/index.js
@@ -7,6 +7,34 @@ import { model, Joi } from '../src/index';
  * Sets up full test using NeDB (inMemory)
  */
 
-describe('integration', () => {
+let testModel;
 
+describe('integration', () => {
+  
+  describe('create model', () => {
+    
+    it('creates a model using the nedb built-in adapter', () => {
+      // Create test model
+      testModel = model.create({
+        // Set adapter
+        adapter: {
+          use: 'nedb',
+          config: {
+            inMemoryOnly: true
+          }
+        },
+        // Set schema
+        schema: {
+          id: Joi.number().integer(),
+          fname: Joi.string().min(3).max(30),
+          lname: Joi.string().min(3).max(30),
+          email: Joi.string().email().min(3).max(30).required()
+        }
+      });
+      // Ensure creation
+      expect(testModel).to.be.an.object;
+    });
+    
+  });
+  
 });

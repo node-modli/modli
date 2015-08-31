9113b6466c9b239541fde721913faa28deb74852
fluidbyte <kent@fluidbyte.net>
2015-08-31 12:23:21 -0400

    Fix extend to prevent cross-model inheritance

--- test/index.spec.js
+++ test/index.spec.js
@@ -61,7 +61,6 @@ describe('integration', () => {
         lname: 'Doe',
         email: 'jdoe@gmail.com'
       };
-      console.log(testModel.create);
       // Create
       testModel.create(testPassData)
         .then((data) => {
